4566b68892a42742a38358e19325988dd8c09c42
LenaPetrenko <54915954+LenaPetrenko@users.noreply.github.com>
2020-10-13 12:37:22 +0300

    .

---
+++ exercises/04_data_structures/task_4_6.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 4.6
+
+Обработать строку ospf_route и вывести информацию на стандартный поток вывода в виде:
+Prefix                10.0.24.0/24
+AD/Metric             110/41
+Next-Hop              10.0.13.3
+Last update           3d18h
+Outbound Interface    FastEthernet0/0
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+"""
+
+ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+
+print_ospf_route = '''
+   Prefix               {}
+   Metric               {}
+   Next-Hop             {}
+   Last update          {}
+   Outbound Interface   {}
+    '''
+
+ospf_route = ospf_route.split()
+
+print(print_ospf_route.format(ospf_route[0],ospf_route[1].strip('[]'),ospf_route[3].rstrip(','),ospf_route[4].rstrip(','),ospf_route[5]))
