c88911f4ad483a7047711cdb08d16c93f81fb56a
LenaPetrenko <54915954+LenaPetrenko@users.noreply.github.com>
2020-10-19 01:19:11 +0300

    .

--- exercises/07_files/task_7_3.py
+++ exercises/07_files/task_7_3.py
@@ -17,12 +17,18 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
-with open('/home/std/python-web/exercises/07_files/CAM_table.txt', 'r') as file_table:
+'''with open('/home/std/python-web/exercises/07_files/CAM_table.txt', 'r') as file_table:
     for line in file_table:
         if line.count('.') is 2:
          line.split(' ')
          vlan = line[0:4]
          mac = line[4:24]
          ports = line [36:] 
-         print_line = '''{}{}{}'''
-         print(print_line.format(vlan, mac, ports))
+         print_line = 
+         print(print_line.format(vlan, mac, ports), end='')'''
+
+import re
+file_table = open('/home/std/python-web/exercises/07_files/CAM_table.txt', 'r')
+for line in file_table:
+    if re.search(r'([0-9A-Fa-f]{4}[.]){2}([0-9A-Fa-f]{4})',line)!=None:
+        print(line.replace('DYNAMIC     ',''), end='')
